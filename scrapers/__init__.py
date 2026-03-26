c1370b57094676a0b2216180c1531d955d2cffce
Lander Stuyts <lander.stuyts@student.kdg.be>
2026-03-26 18:57:50 +0100

    Remove boatscom, yachtworld, botentekoop scrapers (Cloudflare blocked)

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,17 +1,11 @@
 from scrapers.scanboat import ScanboatScraper
-from scrapers.yachtworld import YachtWorldScraper
 from scrapers.boat24 import Boat24Scraper
 from scrapers.theyachtmarket import TheYachtMarketScraper
-from scrapers.botentekoop import BotenTeKoopScraper
 from scrapers.sailboatlistings import SailboatListingsScraper
-from scrapers.boatscom import BoatsComScraper
 
 ALL_SCRAPERS = [
     ScanboatScraper,
-    YachtWorldScraper,
     Boat24Scraper,
     TheYachtMarketScraper,
-    BotenTeKoopScraper,
     SailboatListingsScraper,
-    BoatsComScraper,
 ]
